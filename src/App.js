2fa2911417c2e822ef1eb86333d0e24991874ba8
RilDev <c.public@tutanota.com>
2020-07-31 22:13:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GitHubSVG from './assets/github.svg';
+import LinkedInSVG from './assets/linkedin.svg';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding: 0 20px;
+  text-align: center;
+
+  .photo {
+    border-radius: 50%;
+    overflow: hidden;
+    max-width: 400px;
+    max-height: 400px;
+    box-shadow: 1px 1px 4px #111111;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .links {
+    margin-top: 40px;
+
+    a {
+      &:not(:last-child) {
+        margin-right: 20px;
+      }
+    }
+
+    img {
+      height: 40px;
+    }
+  }
+
+  .title {
+    font-family: 'Roboto Mono', monospace;
+    font-weight: bold;
+  }
+
+  .site-under-construction {
+    font-family: monospace;
+    font-size: 20px;
+    animation: blink 1s infinite;
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 100%;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <AppWrapper>
+      <div className={`photo`}>
+        <img
+          src={`https://avatars2.githubusercontent.com/u/13135150?v=4`}
+          alt={``}
+        />
+      </div>
+      <div className={`links`}>
+        <a href={`https://github.com/RilDev`} target={`_blank`}>
+          <img src={GitHubSVG} alt={''} />
         </a>
-      </header>
-    </div>
+        <a href={`https://www.linkedin.com/in/acyril/`} target={`_blank`}>
+          <img src={LinkedInSVG} alt={''} />
+        </a>
+      </div>
+      <h1 className={`title`}>Welcome to RilDev</h1>
+      <div className={`site-under-construction`}>
+        Site Under Construction...
+      </div>
+    </AppWrapper>
   );
 }
 
